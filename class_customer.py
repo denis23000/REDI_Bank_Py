4aeb1d0aee1c9bb55b661ba533fb8c43a5cf1897
Dzianis <81986373+denis23000@users.noreply.github.com>
2022-12-11 20:33:24 +0100

    adding comments

--- class_customer.py
+++ class_customer.py
@@ -7,13 +7,13 @@ class Customer:
         self.password = password
 
 
-    def set_name(self, new_name):
+    def set_name(self, new_name):                    # functions set are for admin modus.
         self.name = new_name
-    def set_address(self, new_address):
+    def set_address(self, new_address):               # with theese function admin changes information of customers
         self.address = new_address
     def set_phone_number(self, new_phone_number):
         self.phone_number = new_phone_number
 
     def __str__(self):
-       return f"{self.name}, adress: {self.address}, phone number: {self.phone_number}"
+       return f"{self.name}, address: {self.address}, phone number: {self.phone_number}"
 
